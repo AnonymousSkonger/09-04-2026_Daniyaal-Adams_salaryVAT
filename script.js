a12340884f6a7ba91e06b4efc50371388df6b8ed
AnonymousSkonger <daniyaal.adams26@gmail.com>
2026-04-09 12:28:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//Capture following inputs
+function calculateSalary() {
+    //Get values from form
+    let dailyRate = Number(document.getElementById("daily_rate").value);
+    let daysWorked = Number(document.getElementById("days_worked").value);
+
+    //Calculate Salary
+    let grossSalary = dailyRate * daysWorked;
+
+    //VAT
+    let vatRate = 0;
+    if(grossSalary >= 20000){
+        vatRate = 0.20;
+    }
+    else if(grossSalary >= 10000 && grossSalary < 20000){
+        vatRate = 0.10;
+    }
+    else {
+        vatRate = 0;
+    }
+
+    //Calculate VAT
+    let vat = grossSalary * vatRate;
+
+    //Calculate Net Salary
+    let netSalary = grossSalary - vat;
+
+    //Display Results
+    document.getElementById("result").innerHTML = 
+    ("Gross Salary: " + grossSalary + 
+        "<br>VAT: " + vat + 
+        "<br>Salary after VAT: " + netSalary);
+}
